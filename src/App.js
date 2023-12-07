b3ed99c6b0e07fedaac7b8f50f77f799f8c80be8
manimanoj871 <manimanoj10476@gmail.com>
2023-12-07 10:26:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <p>Name : M.Manikandan</p>
+      <p>Department : Information Technology</p>
+      <p>Year : Third Year</p>
+      <p>Register Number : 73772121160</p>
+      <p>Area of Interest: Java Developer </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
